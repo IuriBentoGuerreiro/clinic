9e81973027066b82dda3ec90e2c6d8d96837ea6b
iuri <bentoiuri@gmail.com>
2024-07-09 10:14:50 -0300

    implements patient and dentist

---
+++ src/main/java/com/consultorio/odontologico/model/Dentist.java
@@ -0,0 +1,38 @@
+package com.consultorio.odontologico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "dentist")
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dentist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "specialty")
+    private String specialty;
+    @Column(name = "crm")
+    private String crm;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "working_hours")
+    private String workingHours;
+
+    public static Dentist convert(){
+        //TODO fazer metodo build
+        return null;
+    }
+}
---
+++ src/main/java/com/consultorio/odontologico/model/Patient.java
@@ -0,0 +1,39 @@
+package com.consultorio.odontologico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Table(name = "patient")
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "date_birth")
+    private LocalDate dateBirth;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "email")
+    private String email;
+
+    public static Patient convert(){
+        //TODO fazer metodo build
+        return null;
+    }
+
+}
