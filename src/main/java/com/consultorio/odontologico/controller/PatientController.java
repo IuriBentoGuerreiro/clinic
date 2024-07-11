6435822fdbc3bdb411bba1aae5a00af9886526c0
iuri <bentoiuri@gmail.com>
2024-07-11 15:48:04 -0300

    implements patient CRUD

---
+++ src/main/java/com/consultorio/odontologico/controller/PatientController.java
@@ -0,0 +1,44 @@
+package com.consultorio.odontologico.controller;
+
+import com.consultorio.odontologico.dto.patient.PatientRequest;
+import com.consultorio.odontologico.dto.patient.PatientResponse;
+import com.consultorio.odontologico.service.PatientService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "Patient")
+@RestController
+@RequestMapping("/patient")
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @PostMapping
+    public PatientResponse save(@RequestBody PatientRequest patientRequest){
+        return patientService.save(patientRequest);
+    }
+
+    @GetMapping
+    public List<PatientResponse> findAll(){
+        return (patientService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public PatientResponse findById(@PathVariable Integer id){
+        return PatientResponse.convert(patientService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public PatientResponse update(@PathVariable Integer id, @RequestBody PatientRequest patientRequest){
+        return PatientResponse.convert(patientService.update(id, patientRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        patientService.deleteById(id);
+    }
+}
--- src/main/java/com/consultorio/odontologico/service/PatientService.java
+++ src/main/java/com/consultorio/odontologico/service/PatientService.java
@@ -1,20 +1,60 @@
 package com.consultorio.odontologico.service;
 
+import com.consultorio.odontologico.dto.patient.PatientRequest;
+import com.consultorio.odontologico.dto.patient.PatientResponse;
+import com.consultorio.odontologico.model.Appointment;
 import com.consultorio.odontologico.model.Patient;
 import com.consultorio.odontologico.repository.PatientRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.webjars.NotFoundException;
+
+import java.util.List;
 
 @Service
 public class PatientService {
 
+    private final PatientRepository patientRepository;
+
     @Autowired
-    private PatientRepository patientRepository;
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository = patientRepository;
+    }
+
+    public PatientResponse save(PatientRequest patientRequest){
+        var patient = patientRepository.save(Patient.builder()
+                        .name(patientRequest.getName())
+                        .phone(patientRequest.getPhone())
+                        .email(patientRequest.getEmail())
+                        .address(patientRequest.getAddress())
+                        .dateBirth(patientRequest.getDateBirth())
+                        .appointments(patientRequest.getAppointment().stream()
+                                .map(Appointment::convert).toList())
+                .build());
+
+        return PatientResponse.convert(patient);
+    }
+
+    public List<PatientResponse> findAll(){
+        return patientRepository.findAll().stream()
+                .map(PatientResponse::convert).toList();
+    }
 
     public Patient findById(Integer id){
         return patientRepository.findById(id).orElseThrow(
                 () -> new NotFoundException("Not found")
         );
+    }
+
+    public Patient update(Integer id, PatientRequest patientRequest){
+        var patientSave = findById(id);
+        BeanUtils.copyProperties(patientRequest, patientSave, "id");
+        return patientRepository.save(patientSave);
+    }
+
+    public void deleteById(Integer id){
+        patientRepository.deleteById(id);
     }
 }
