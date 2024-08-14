46d5f231edb9e7cabce178f5dfa059c38551fe8d
iuri <bentoiuri@gmail.com>
2024-08-14 15:34:48 -0300

    implements exception methods

--- src/main/java/com/consultorio/odontologico/handler/RestExceptionHandler.java
+++ src/main/java/com/consultorio/odontologico/handler/RestExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.consultorio.odontologico.handler;
 
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.webjars.NotFoundException;
+
+import java.sql.SQLException;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -20,4 +23,25 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     private ResponseEntity<String> constraintViolationExceptionHandler(ConstraintViolationException exception){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
+
+    @ExceptionHandler(NullPointerException.class)
+    private ResponseEntity<String> nullPointerExceptionHandler(NullPointerException exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Null pointer exception occurred: " + exception.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    private ResponseEntity<String> illegalArgumentExceptionHandler(IllegalArgumentException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid argument: " + exception.getMessage());
+    }
+
+    @ExceptionHandler(SQLException.class)
+    private ResponseEntity<String> sqlExceptionHandler(SQLException exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Database error: " + exception.getMessage());
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    private ResponseEntity<String> dataAccessExceptionHandler(DataAccessException exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Data access error: " + exception.getMessage());
+    }
+
 }
